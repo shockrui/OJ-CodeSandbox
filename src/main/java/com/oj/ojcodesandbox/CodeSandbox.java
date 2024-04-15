aff6b861ac7fb383038278e970ecea4d0d7e36bf
shockrui <13578891+shockrui@user.noreply.gitee.com>
2024-04-15 19:49:49 +0800

    代码沙箱，java原生实现测试

---
+++ src/main/java/com/oj/ojcodesandbox/CodeSandbox.java
@@ -0,0 +1,19 @@
+package com.oj.ojcodesandbox;
+
+
+import com.oj.ojcodesandbox.model.ExecuteCodeRequest;
+import com.oj.ojcodesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
